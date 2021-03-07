d147fd0394c8b2dba6ba1965651926989f043ad5
Sam Parsons <samparsons269@gmail.com>
2021-03-07 15:51:08 -0500

    updated file /folder structure

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const getStdin = require('get-stdin');
+const { readFileSync, writeFileSync } = require('fs');
+const lib = require('../dist/index').default;
+
+const { version } = require('../package.json');
+
+commander
+  .version(version)
+  .usage('[options] <file ...>')
+  //.option('--some-option', 'This is an example of an option')
+  .parse(process.argv);
+
+// const { args: files } = commander;
+
+// getStdin().then(stdin => {
+//   if (!files.length && !stdin) {
+//     commander.outputHelp();
+//     process.exit(0);
+//   }
+
+//   if (stdin) {
+//     process.stdout.write(
+//       lib(stdin)
+//     );
+//   } else {
+//     files.forEach(file => {
+//       process.stdout.write(
+//         lib(
+//           readFileSync(file, 'utf8')
+//         )
+//       );
+//     });
+//   }
+// });
---
+++ test/index.js
@@ -0,0 +1,5 @@
+import library from '../lib/index';
+import path from 'path';
+import assert from 'assert';
+
+import { readFileSync } from 'fs';
