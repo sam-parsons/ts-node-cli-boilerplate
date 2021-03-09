464ce7f738ae3e65ca659d58d058ab101b452687
Sam Parsons <samparsons269@gmail.com>
2021-03-09 00:00:56 -0500

    basic jest testing

---
+++ __tests__/index.js
@@ -0,0 +1,5 @@
+import lib from '../lib';
+
+test('first', () => {
+  expect(lib('text')).toEqual('text');
+})
--- test/index.js
+++
@@ -1,5 +0,0 @@
-import library from '../lib/index';
-import path from 'path';
-import assert from 'assert';
-
-import { readFileSync } from 'fs';
