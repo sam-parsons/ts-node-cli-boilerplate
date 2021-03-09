a5712d801898554aaa24439fb4e5df8377291c9e
Sam Parsons <samparsons269@gmail.com>
2021-03-09 16:26:43 -0500

    working cli script update

--- bin/cli.js
+++ bin/cli.js
@@ -1,37 +1,5 @@
 #!/usr/bin/env node
 
-// const commander = require('commander');
-// const getStdin = require('get-stdin');
-// const { readFileSync, writeFileSync } = require('fs');
-// const lib = require('../dist/index').default;
+const lib = require('../dist/index.js').default;
 
-// const { version } = require('../package.json');
-
-// commander
-//   .version(version)
-//   .usage('[options] <file ...>')
-//.option('--some-option', 'This is an example of an option')
-// .parse(process.argv);
-
-// const { args: files } = commander;
-
-// getStdin().then(stdin => {
-//   if (!files.length && !stdin) {
-//     commander.outputHelp();
-//     process.exit(0);
-//   }
-
-//   if (stdin) {
-//     process.stdout.write(
-//       lib(stdin)
-//     );
-//   } else {
-//     files.forEach(file => {
-//       process.stdout.write(
-//         lib(
-//           readFileSync(file, 'utf8')
-//         )
-//       );
-//     });
-//   }
-// });
+console.log('lib return value', lib(process.argv[2]));
