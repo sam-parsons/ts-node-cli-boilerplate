b82ebc06d101c140b33f15c9987eaa25bcbd6654
Sam Parsons <samparsons269@gmail.com>
2021-03-09 00:20:05 -0500

    webpack build set up

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./lib/index.js",
+  mode: "production",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules"]
+  }
+};
